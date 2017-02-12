3df284c2cbd3835585baa0e03cf64c673137b0a1
Manav Kataria <manavkataria.iitb@gmail.com>
2017-02-12 13:24:32 -0800

    ROI Masking Successful

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import matplotlib
 import inspect
+import cv2
+import numpy as np
 
-from settings import DEBUG, DISPLAY
+from settings import DEBUG, DISPLAY, ROI_bbox, ROI
 
 matplotlib.use('TkAgg')  # MacOSX Compatibility
 matplotlib.interactive(True)
@@ -38,10 +40,24 @@ def imcompare(image1, image2, msg1='Image1', msg2='Image2', cmap1=None, cmap2=No
 
     # Plot the result
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-    f.tight_layout()
     ax1.imshow(image1, cmap=cmap1)
     ax1.set_title(msg1, fontsize=30)
     ax2.imshow(image2, cmap=cmap2)
     ax2.set_title(msg2, fontsize=30)
     plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+    # title = f.suptitle(msg1)
+    f.tight_layout()
+    # title.set_y(0.75)
     plt.show(block=True)
+
+
+def warper(img, src, dst):
+    # Compute and apply perpective transform
+    img_size = (img.shape[1], img.shape[0])
+    M = cv2.getPerspectiveTransform(src, dst)
+    Minv = cv2.getPerspectiveTransform(dst, src)
+
+    # keep same size as input image
+    # warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)
+    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+    return warped, M, Minv
